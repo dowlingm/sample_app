7c159d51546939b0629ba3e8562c7dfc51a26401
Michelle Dowling <dowlingm@mail.gvsu.edu>
2013-05-13 14:22:10 -0400

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -17,10 +17,12 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
 
   gem 'rspec-rails', '2.11.0'
-  # gem 'guard-rspec', '1.2.1'
-  # gem 'guard-spork', '1.2.0'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
   # gem 'childprocess', '0.3.6'
-  # gem 'spork', '0.9.2'
+  gem 'spork', '0.9.2'
+
+  gem 'wdm', '~> 0.1.0'
 end
 
 
@@ -42,6 +44,17 @@ group :test do
   # gem 'cucumber-rails', '1.2.1', :require => false
   # gem 'database_cleaner', '0.7.0'
   # gem 'launchy', '2.1.0'
+
+  # Test gems on Windows
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0'
+
+  # Test gems on Linux
+  # gem 'rb-inotify', '0.8.8'
+  # gem 'libnotify', '0.5.9'
+
+  # Test gems on Macintosh OS X
   # gem 'rb-fsevent', '0.9.1', :require => false
   # gem 'growl', '1.0.3'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,16 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-		:text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+
+    it "should not have the right title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "Home")
     end
   end
 
@@ -43,6 +49,18 @@ describe "StaticPages" do
 		:text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+		:text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
-
-
